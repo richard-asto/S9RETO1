861cc48665afff6e2add48ec5267e6fd12df7a17
richard-asto <SV09681015@idat.pe>
2025-11-08 15:34:25 -0500

    Respuesta 7

--- script.js
+++ script.js
@@ -168,3 +168,36 @@ for (let i = 1; i <= 50; i++) {
 }
 
 //==========================================================================//
+
+// 7. Escriba un programa de JavaScript que itera enteros del 1 al 100.
+// Pero para múltiplos de tres, imprima "Fizz" en lugar del número y para
+// múltiplos de cinco imprima "Buzz". Para números múltiplos de tres y
+// cinco, imprima "FizzBuzz".
+// Salida de muestra:
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz
+// .
+// .
+// .
+// 14
+// FizzBuzz
+// 16
+// .
+// .
+
+// Respuesta:
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+    else if (i % 3 === 0) console.log("Fizz");
+    else if (i % 5 === 0) console.log("Buzz");
+    else console.log(i);
+}
+
+//==========================================================================//
