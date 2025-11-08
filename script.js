c3148b41a32dc2fa49498fed461753c92dffd2cb
richard-asto <SV09681015@idat.pe>
2025-11-08 15:43:07 -0500

    Respuesta 8

--- script.js
+++ script.js
@@ -201,3 +201,21 @@ for (let i = 1; i <= 100; i++) {
 }
 
 //==========================================================================//
+
+// 8. Solicitar al usuario una calificación (entre 1 y 10). Luego se debe
+// comprobar que el número efectivamente esté en ese rango, si no lo está
+// imprima un mensaje de error. Si lo está, imprima “reprobado” si la
+// calificación es inferior a 6, “regular” si está entre 6 y 8, “bien” si
+// es 9, y por último, “excelente” si es 10.
+
+//Respuesta:
+const nota = parseInt(prompt("Ingrese una calificación (1-10):"));
+console.log("Nota: " + nota)
+if (nota < 1 || nota > 10) {
+    console.log("Error: la calificación debe estar entre 1 y 10");
+} else if (nota < 6) console.log("Reprobado");
+else if (nota <= 8) console.log("Regular");
+else if (nota === 9) console.log("Bien");
+else console.log("Excelente");
+
+//==========================================================================//
