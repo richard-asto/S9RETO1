6e0f1570cbf57a661c828c8cff9feb50cb7bcee2
richard-asto <SV09681015@idat.pe>
2025-11-08 15:20:40 -0500

    Respuesta 3

--- script.js
+++ script.js
@@ -41,3 +41,36 @@ console.log("Arreglo: {" + entradaB + "}")
 console.log("Suma de números pares:", sumaPares);
 
 //==========================================================================//
+
+// 3. Escriba un programa en JavaScript para mostrar el nombre del libro y el
+//  nombre del autor de los libros que tengan el estado de lectura “true”.
+// const library = [
+// {
+// author: 'Bill Gates',
+// title: 'The Road Ahead',
+// readingStatus: true },
+// {
+// author: 'Steve Jobs',
+// title: 'Walter Isaacson',
+// readingStatus: true,},
+// {
+// author: 'Suzanne Collins',
+// title: 'Mockingjay: The Final Book of The Hunger Games',
+// readingStatus: false,
+// },
+// ];
+
+//Respuesta:
+const library = [
+    { author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true },
+    { author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true },
+    { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', readingStatus: false },
+];
+
+library.forEach(function (book) {
+    if (book.readingStatus === true) {
+        console.log("Libro: " + book.title + " - Autor: " + book.author);
+    }
+});
+
+//==========================================================================//
